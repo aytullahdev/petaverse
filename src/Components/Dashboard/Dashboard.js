6d1452588021b2b710336dee665cf0a4591b348c
unknown <ayt.ullah.dev@gmail.com>
2022-04-04 22:34:12 +0600

    chart added

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { LineChart, Line, XAxis, BarChart, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, Bar } from 'recharts';
 
 const Dashboard = () => {
    const  data=[
@@ -41,29 +41,48 @@ const Dashboard = () => {
         }
     ]
     return (
-       <div className='grid grid-cols-1 md:grid-cols-2 gap-4 bg-red-400'>
-         <div className=' bg-white'>
-       
+       <div className='grid grid-cols-1 pl-2  md:grid-cols-2 gap-4 overflow-x-hidden '>
+         <div className=' bg-white shadow-md relative  '>
+         <ResponsiveContainer width="100%"  height={400}>
         <LineChart
-          width={500}
-          height={300}
           data={data}
+          
           margin={{
             top: 5,
             bottom: 5,
           }}
         >
           <CartesianGrid strokeDasharray="3 3" />
+          <Legend />
           <XAxis dataKey="month" />
           <YAxis />
           <Tooltip />
-          <Legend />
+         
           <Line type="monotone" dataKey="sell" stroke="#82ca9d" />
         </LineChart>
-    
+     </ResponsiveContainer>
          </div>
-         <div>
-
+         <div className=' bg-white shadow-md '>
+         <ResponsiveContainer width="100%" height={400}>
+         <BarChart
+      data={data}
+      margin={{
+        top: 5,
+        right: 30,
+        left: 20,
+        bottom: 5
+      }}
+    >
+     
+      <XAxis dataKey="month" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Bar dataKey="sell" fill="#8884d8" />
+     
+    </BarChart>
+    </ResponsiveContainer>
+         
          </div>
          
        </div>
